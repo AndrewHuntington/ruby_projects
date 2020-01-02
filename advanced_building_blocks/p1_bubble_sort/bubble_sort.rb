0f9b2faef90f0783268bb76f46d551194d85d4a3
AndrewHuntington <andrew.huntington@gmail.com>
2020-01-02 18:35:13 +0900

    add and finish bubble_sort_by()

--- advanced_building_blocks/p1_bubble_sort/bubble_sort.rb
+++ advanced_building_blocks/p1_bubble_sort/bubble_sort.rb
@@ -5,9 +5,8 @@ def bubble_sort array
 
   while !sorted && x < array.length - 1
     times_swapped = 0
-    puts "1st #{x}"
 
-    (0..array.length - n).each do |i|
+    (0...array.length - n).each do |i|
       if (array[i] <=> array[i+1]) == 1
         temp = array[i]
         array[i] = array[i+1]
@@ -22,12 +21,42 @@ def bubble_sort array
       n += 1
       x += 1
     end
-
-    puts "2nd #{x}"
-    puts "is sorted? #{sorted}"
   end
 
   array
 end
 
 bubble_sort([4,3,78,2,0,2])
+
+
+def bubble_sort_by array
+  sorted = false
+  x = 0
+  n = 1
+
+  while !sorted && x < array.length - 1
+    times_swapped = 0
+
+    (0...array.length - n).each do |i|
+      if yield(array[i], array[i+1]) > 0
+        temp = array[i]
+        array[i] = array[i+1]
+        array[i+1] = temp
+        times_swapped += 1
+      end
+    end
+
+    if times_swapped == 0
+      sorted = true
+    else
+      n += 1
+      x += 1
+    end
+  end
+
+  array
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
