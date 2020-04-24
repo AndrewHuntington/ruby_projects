10ddecfcbab216333fe8973cb09937bf393aeb83
Andrew Huntington <andrew.huntington@gmail.com>
2020-04-24 15:05:51 +0900

    finish Fibonacci.fibs

--- project_recursion/fibonacci/fibonacci.rb
+++ project_recursion/fibonacci/fibonacci.rb
@@ -3,9 +3,19 @@ module Fibonacci
   # Fn = Fn-1 + Fn-2 (given the first two values are 0 and 1)
   # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55...
 
-  def self.fibs(n=0)
+  def self.fibs(number)
     # uses iteration
-    puts "hi from fibs!"
+    sequence = [0, 1]
+
+    return [] if number <= 0
+    return sequence[0..number-1] if number <= 2
+
+
+    2.upto(number-1) do |n|
+      sequence << (sequence[n-1]) + (sequence[n-2])
+    end
+
+    sequence
   end
 
   def self.fibs_rec(n=0)
@@ -15,5 +25,5 @@ module Fibonacci
 
 end
 
-Fibonacci.fibs
-Fibonacci.fibs_rec
+# Fibonacci.fibs(10) # => [0, 1, 1, 2, 3, 5, 8, 13, 21, 34] OK!
+# Fibonacci.fibs_rec(10) # => [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
