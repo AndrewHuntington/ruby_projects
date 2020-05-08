5dd091220dc3a466c82e0cafb5263e6332d0f497
Andrew Huntington <andrew.huntington@gmail.com>
2020-05-08 16:56:41 +0900

    included recursive solution in fibonacci.rb

--- project_recursion/fibonacci/fibonacci.rb
+++ project_recursion/fibonacci/fibonacci.rb
@@ -19,14 +19,28 @@ module Fibonacci
 
     def fibs_rec(n)
       # uses recursion
-      return [] if n == 0
-      return [0] if n == 1
-      return [0,1] if n == 2
+      return 0 if n == 0
+      return 1 if n == 1
 
       fibs_rec(n-1) + fibs_rec(n-2)
+    end
+
+    def fibs_assist
+      # this is the only way I can figure out to get the fib_sequence
+      # into an array using the recursive function. Maybe it's cheating...
+      print "Enter a number: "
+      n = gets.to_i
+
+      fib_sequence = []
+
+      n.times do |n|
+        fib_sequence[n] = fibs_rec(n)
+      end
+
+      fib_sequence
     end
   end
 end
 
 # Fibonacci.fibs(10) # => [0, 1, 1, 2, 3, 5, 8, 13, 21, 34] OK!
-# p Fibonacci.fibs_rec(10) # => [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+p Fibonacci.fibs_assist # => [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
