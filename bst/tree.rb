44c3dba67f23fe524c8eaf2819540663781d345c
Andrew Huntington <andrew.huntington@gmail.com>
2020-05-27 00:10:15 +0900

    finish Tree#delete in tree.rb

--- bst/tree.rb
+++ bst/tree.rb
@@ -63,31 +63,55 @@ class Tree
     # Must deal with several cases such as when a node has children or not
     pointer = @root
 
-    # update the array to include new values
-    @array.delete(value)
-    
-    # 3 cases:
-    # case 1: leaf node (no child nodes)
-    # case 2: node w/1 child
-    # case 3: node w/2 children (including root)
-
     # find the node before the value to be deleted
-    while pointer.left.data != value && pointer.right.data != value
-      if value < pointer.data
-        pointer = pointer.left
-      else
-        pointer = pointer.right
+    if value != pointer.data  # don't run if value == root
+      while pointer.left.data != value && pointer.right.data != value
+        if value < pointer.data
+          pointer = pointer.left
+        else
+          pointer = pointer.right
+        end
       end
     end
 
-    # handle case 1
-    if pointer.left.data == value
-      pointer.left = nil
-    elsif pointer.right.data == value
-      pointer.right = nil
+    # 3 cases:
+    # case 1: leaf node (no child nodes)
+    # case 2: node w/1 child
+    # case 3: node w/2 children (including root)    
+    if pointer.left.data == value && 
+      pointer.left.left.nil? && pointer.left.right.nil?         # handle case 1
+        pointer.left = nil
+    elsif pointer.right.data == value &&
+      pointer.right.left.nil? && pointer.right.right.nil?
+        pointer.right = nil
+    elsif pointer.left.data == value && pointer.left.right.nil? # handle case 2
+      pointer.left = pointer.left.left
+    elsif pointer.left.data == value && pointer.left.left.nil?
+      pointer.left = pointer.left.right
+    elsif pointer.right.data == value && pointer.right.right.nil? 
+      pointer.right = pointer.right.left
+    elsif pointer.right.data == value && pointer.right.left.nil?
+      pointer.right = pointer.right.right
+    else                                                        # handle case 3
+      pointer = find(value)
+
+      right_min = pointer.right
+
+      until right_min.left.nil?
+        if right_min > right_min.left
+          right_min = right_min.left
+        end
+      end
+
+      delete(right_min.data)
+      pointer.data = right_min.data
     end
 
+    # update the array to include new values
+    @array.delete(value)
+
     self
+
     # this works, but may not be in the spirit of the challenge...
     # @root = build_tree(@array)
   end
@@ -116,34 +140,28 @@ class Tree
 
   # breadth-first traversal
   def level_order
-    # Accepts a block
-    # Traverses the tree in breadth-first level order and yields each node to the provided block
-    # Returns an array of values if no block is given
-  end
+    # Returns an array of values in breadth-first level order
+  end 
 
 
   # depth-first traversal methods
-  # look into dynamically declaring the three methods using metaprogamming techniques like #define_method.
 
   # DLR
   def preorder
-    # Accepts a block
-    # Traverses the tree in it's respective depth-first order and yields each node to the provided bloc
-    # Returns an array of values if no block is given
+    # Traverses the tree
+    # Returns an array of values
   end
 
   # LDR
   def inorder
-    # Accepts a block
-    # Traverses the tree in it's respective depth-first order and yields each node to the provided block
-    # Returns an array of values if no block is given
+    # Traverses the tree
+    # Returns an array of values
   end
 
   #LRD
   def postorder
-    # Accepts a block
-    # Traverses the tree in it's respective depth-first order and yields each node to the provided block
-    # Returns an array of values if no block is given
+    # Traverses the tree
+    # Returns an array of values
   end
 
   def depth
